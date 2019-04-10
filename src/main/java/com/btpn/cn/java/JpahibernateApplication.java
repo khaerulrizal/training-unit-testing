213e88bc46fc2d49ab6072f6932d2068e8acfe91
Khaerul Rizal <khaerul.rizal@btpn.com>
2019-04-10 10:33:02 +0700

    Rename Artifact ID

--- src/main/java/com/btpn/cn/java/JpahibernateApplication.java
+++ src/main/java/com/btpn/cn/java/JpahibernateApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories(basePackages = "com.btpn.jpahibernate.jpahibernate.repository")
+@EnableJpaRepositories(basePackages = "com.btpn.cn.java.repository")
 @SpringBootApplication
 public class JpahibernateApplication {
 
